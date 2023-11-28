8483f504db04bed2adb88bf8b48584f6ca4e68b7
Ali <ali.rezai27461@gmail.com>
2023-11-28 13:49:59 +0100

    vad är det som händer"

--- JS-grund/ovning/objcet/main.js
+++ JS-grund/ovning/objcet/main.js
@@ -14,16 +14,3 @@ window.onload = function () {
   person1.length = 167;
   console.log(person1);
 };
-
-/* const person1 = new Person("Ali", 23, "Rezai", 167);
-const person2 = new Person("Reza", 25, "Jafari", 170);
-const person3 = new Person("Mamal", 23, "Rozi", 190);
-const person4 = new Person("Pedram", 23, "Salar", 187); */
-
-/*
-const persons = [person1, person2, person3, person4];
-
-for (let i = 0; i < persons.length; i++) {
-  console.log(persons[i]);
-}
- */
